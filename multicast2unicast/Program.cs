efb6345eefaa9298f731ff8f7faa7282b13fd362
veso266 <veso266@gmail.com>
2018-10-13 22:51:35 +0200

    it doesn't yet work

---
+++ multicast2unicast/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multicast2unicast
+{
+    class Program
+    {
+        static protected UdpClient listener;
+        static protected IPAddress localAddress;
+        static protected IPAddress listenAddress;
+        static protected int listenPort = 5002;
+
+        static protected IPEndPoint inPoint;
+
+        byte[] data;
+
+        // SLO 3: 232.2.1.41:5002
+
+        public static void MulticastListen(string ip, int port, string interface2bind)
+        {
+            IPAddress.TryParse(ip, out listenAddress); //Our program that we want to recieve
+            listenPort = port;
+            IPAddress.TryParse(interface2bind, out localAddress); //To know to which interface to bind
+            
+            //IPAddress.TryParse("192.168.88.12", out localAddress); //To know to which interface to bind
+            //IPAddress.TryParse("232.2.1.41", out listenAddress); //Our program that we want to recieve
+            listener = new UdpClient();
+            listener.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            inPoint = new IPEndPoint(localAddress, listenPort);
+            listener.Client.Bind(inPoint);
+            listener.JoinMulticastGroup(listenAddress);
+        }
+
+        public void loop()
+        {
+            bool done = false;
+            try
+            {
+                while (!done)
+                {
+                    byte[] bytes = listener.Receive(ref inPoint);
+                    //Console.WriteLine("Received Multicast from  {0} : length {1}\n", listenAddress.ToString(), bytes.Length);
+                    //this.send(bytes);
+                    //recieveBytes(bytes);
+                    //Lets get to buisness now
+                    rtp _rtp = new rtp(bytes, bytes.Length);
+                    //_rtp.RTP_check();
+
+                    recieveBytes(_rtp.RTP_process(1));
+                    /*
+                    if (_rtp.RTPOK == 0)
+                    {
+                        Console.WriteLine(_rtp.buf[0]);
+                    }
+                    */
+
+
+
+
+                    //Lets get to buisness now
+
+
+
+
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                listener.Close();
+                //sender.Close();
+            }
+        }
+
+        void recieveBytes(byte[] data2fill)
+        {
+            data = data2fill;
+        }
+
+        static void Main(string[] args)
+        {
+            Program _Program = new Program();
+            //rtp _rtp = new rtp();
+            MulticastListen("232.2.1.41", 5002, "192.168.88.12"); //SLO 3 (udp)
+            MulticastListen("232.2.201.53", 5003, "192.168.88.12"); //SLO 1 (rtp)
+            _Program.loop(); //To recieve packest continuously
+            //_rtp.RTP_check(_Program.data, _Program.data.Length);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ multicast2unicast/Servers/TCPServer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace multicast2unicast
+{
+    class TCPServer
+    {
+        public TCPServer(string _ip, int _port, byte[] data)
+        {
+            IPAddress ip;
+            IPAddress.TryParse(_ip, out ip);
+            int port = _port;
+            Server(port, data);
+        }
+        public void Server(int port, byte[] data)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, port);
+            // we set our IP address as server's address, and we also set the port: 9999
+
+            server.Start();  // this will start the server
+
+            while (true)   //we wait for a connection
+            {
+                TcpClient client = server.AcceptTcpClient();  //if a connection exists, the server will accept it
+
+                NetworkStream ns = client.GetStream(); //networkstream is used to send/receive messages
+
+                byte[] hello = new byte[100];   //any message must be serialized (converted to byte array)
+                hello = Encoding.Default.GetBytes("hello world");  //conversion string => byte array
+
+                //ns.Write(hello, 0, hello.Length);     //sending the message
+                ns.Write(data, 0, data.Length);     //sending the message
+
+                while (client.Connected)  //while the client is connected, we look for incoming messages
+                {
+                    Console.WriteLine("Client Connected");
+                    //ns.Write(data, 0, data.Length);
+                }
+            }
+        }
+    }
+}
